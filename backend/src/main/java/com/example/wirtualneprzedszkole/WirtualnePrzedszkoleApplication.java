c5c154de7ea5b9955fa46b0cb020810563f18789
LordIsak <60114149+LordIsak@users.noreply.github.com>
2022-05-13 12:07:47 +0200

    create project

---
+++ backend/src/main/java/com/example/wirtualneprzedszkole/WirtualnePrzedszkoleApplication.java
@@ -0,0 +1,13 @@
+package com.example.wirtualneprzedszkole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WirtualnePrzedszkoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WirtualnePrzedszkoleApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/example/wirtualneprzedszkole/WirtualnePrzedszkoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wirtualneprzedszkole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WirtualnePrzedszkoleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
