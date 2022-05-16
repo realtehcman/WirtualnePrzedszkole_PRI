ea921488ceb100797975fb817354f98ff0f20ae7
LordIsak <60114149+LordIsak@users.noreply.github.com>
2022-05-16 11:35:26 +0200

    add Patent entity

---
+++ backend/src/main/java/com/example/wirtualneprzedszkole/model/Parent.java
@@ -0,0 +1,26 @@
+package com.example.wirtualneprzedszkole.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.net.URL;
+
+@Entity
+@Getter
+@Setter
+public class Parent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    //private URL pictureURL;
+    private String firstName;
+    private String lastName;
+    private String birthDate;
+    private String phoneNumber;
+    private String email;
+    private String Address;
+}
