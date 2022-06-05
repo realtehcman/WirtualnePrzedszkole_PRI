2dc791412eae800d525a52b90aaf3bb14c89e932
Mateusz Naszko <60114149+LordIsak@users.noreply.github.com>
2022-06-05 21:04:38 +0200

    fixes to valid

--- backend/src/main/java/com/example/wirtualneprzedszkole/validation/ValidPassword.java
+++ backend/src/main/java/com/example/wirtualneprzedszkole/validation/ValidPassword.java
@@ -8,11 +8,12 @@ import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
 import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Documented
 @Constraint(validatedBy = PasswordConstraintValidator.class)
-@Target({ FIELD, ANNOTATION_TYPE })
+@Target({ TYPE, FIELD, ANNOTATION_TYPE })
 @Retention(RUNTIME)
 public @interface ValidPassword {
     String message() default "Invalid Password";
