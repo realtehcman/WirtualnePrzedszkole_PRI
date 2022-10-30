62a91b16bcbfaae004f1766c6f18783c994a516d
Mateusz Naszko <60114149+LordIsak@users.noreply.github.com>
2022-10-30 16:31:34 +0100

    test merge'a

--- backend/src/main/java/com/example/wirtualneprzedszkole/controller/LoginController.java
+++ backend/src/main/java/com/example/wirtualneprzedszkole/controller/LoginController.java
@@ -4,7 +4,7 @@ import com.example.wirtualneprzedszkole.model.dto.LoginDto;
 import org.springframework.web.bind.annotation.*;
 
 
-/*@CrossOrigin(origins = "http://localhost:3000/", exposedHeaders = {"authorization"})*/
+@CrossOrigin(origins = "http://localhost:3000/", exposedHeaders = {"authorization"})
 
 @RestController
 public class LoginController {
