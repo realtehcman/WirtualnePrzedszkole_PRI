ce760eece6af26de3d6325566e7c16afae58851a
Max <max.dmytrenko@capgemini.com>
2022-11-18 11:35:51 +0100

    fix

--- frontend/wirtualneprzedszkole/src/components/sidebar/sidebar.js
+++ frontend/wirtualneprzedszkole/src/components/sidebar/sidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import {React, useState, useEffect} from "react";
 import "./sidebar.scss"
 import FamilyRestroomIcon from '@mui/icons-material/FamilyRestroom';
 import GroupsIcon from '@mui/icons-material/Groups';
@@ -12,69 +12,133 @@ import { useNavigate } from "react-router-dom";
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import MarkChatReadIcon from '@mui/icons-material/MarkChatRead';
 import RateReviewIcon from '@mui/icons-material/RateReview';
+import "./sidebar.scss";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import { IconContext } from "react-icons";
+// import MediaQuery from "react-responsive";
+import { isMobile } from "react-device-detect";
+import { BrowserView, MobileView } from "react-device-detect";
+import useWindowDimensions from "./useWindowDimensions.js";
+
+
 
 const Sidebar = () => {
-    const navigate = useNavigate();
-    return (
-        <div className="sidebar">
-            <div className="top">
-                <span className="logo">Przedszkole nr 25</span>
-            </div>
+  const { height, width } = useWindowDimensions();
+  const phoneMaxWidth = 480;
+  // const screenSize = width;
+  // const  = width;
+  const navigate = useNavigate();
+  var [sidebar, setSidebar] = useState(true);
+  //const showSidebar = () => setSidebar(!sidebar);
+  const screenSize = useEffect(() => {
+    window.addEventListener("resize", () => {
+      const myWidth = window.innerWidth;
+      console.log("my width :::", myWidth);
+    });
+  }, [window]);
+  
+  return (
+    <>
+      <div className="sidebar">
+        <div className="center">
+          <span
+            onClick={() => navigate("#", { replace: true })}
+            className="menu-bars"
+          >
+            <FaIcons.FaBars onClick={() => setSidebar(!sidebar)} />
+            <hr />
+          </span>
+
+          <div className={sidebar ? "sidebar" : "sidebar-hidden"}>
+            <div className="center">{/* <div className="navbar"></div> */}</div>
             <div className="center">
-                <ul>
-                    <li>
-                        <AccountBoxIcon className="icon"/>
-                        <span onClick={() => navigate('/home', { replace: true })}> Profil</span>
-                    </li>
-                    <li>
-                        <FamilyRestroomIcon className="icon"/>
-                        <span onClick={() => navigate('/users', { replace: true })}> Rodzice</span>
-                    </li>
-                    <li>
-                        <GroupsIcon className="icon"/>
-                        <span  onClick={() => navigate('/groups', { replace: true })}>Grupy</span>
-                    </li>
-                    <li>
-                        <ChildCareIcon className="icon"/>
-                        <span  onClick={() => navigate('/children', { replace: true })}>Dzieci</span>
-                    </li>
-                    <li>
-                        <MessageIcon className="icon"/>
-                        <span  onClick={() => navigate('/Message', { replace: true })}>Wiadomości</span>
-                    </li>
-                    <li>
-                        <RateReviewIcon className="icon"/>
-                        <span  onClick={() => navigate('/SendMessage', { replace: true })}>Utwórz</span>
-                    </li>
+              <ul>
+                <li>
+                  <AccountBoxIcon className="icon" />
+                  <span onClick={() => navigate("/home", { replace: true })}>
+                    {" "}
+                    Profil
+                  </span>
+                </li>
+                <li>
+                  <FamilyRestroomIcon className="icon" />
+                  <span onClick={() => navigate("/users", { replace: true })}>
+                    {" "}
+                    Rodzice
+                  </span>
+                </li>
+                <li>
+                  <GroupsIcon className="icon" />
+                  <span onClick={() => navigate("/groups", { replace: true })}>
+                    Grupy
+                  </span>
+                </li>
+                <li>
+                  <ChildCareIcon className="icon" />
+                  <span
+                    onClick={() => navigate("/children", { replace: true })}
+                  >
+                    Dzieci
+                  </span>
+                </li>
+                <li>
+                  <MessageIcon className="icon" />
+                  <span onClick={() => navigate("/Message", { replace: true })}>
+                    Wiadomości
+                  </span>
+                </li>
+                <li>
+                  <RateReviewIcon className="icon" />
+                  <span
+                    onClick={() => navigate("/SendMessage", { replace: true })}
+                  >
+                    Utwórz
+                  </span>
+                </li>
 
-                    <li>
-                        <MarkChatReadIcon className="icon"/>
-                        <span  onClick={() => navigate('/SentMessage', { replace: true })}>Wysłane</span>
-                    </li>
+                <li>
+                  <MarkChatReadIcon className="icon" />
+                  <span
+                    onClick={() => navigate("/SentMessage", { replace: true })}
+                  >
+                    Wysłane
+                  </span>
+                </li>
 
-                    <li>
-                        <FeedIcon className="icon"/>
-                        <span>Aktualości</span>
-                    </li>
-                    <li>
-                        <SupervisorAccountIcon className="icon"/>
-                        <span>Portal rodzica</span>
-                    </li>
-                    <li>
-                        <SchoolIcon className="icon"/>
-                        <span>Baza wiedzy</span>
-                    </li>
-                    <li>
-                        <CollectionsIcon className="icon"/>
-                        <span>Galeria</span>
-                    </li>
-
-
-                </ul>
+                <li>
+                  <FeedIcon className="icon" />
+                  <span>Aktualości</span>
+                </li>
+                <li>
+                  <SupervisorAccountIcon className="icon" />
+                  <span>Portal rodzica</span>
+                </li>
+                <li>
+                  <SchoolIcon className="icon" />
+                  <span>Baza wiedzy</span>
+                </li>
+                <li>
+                  <CollectionsIcon className="icon" />
+                  <span>Galeria</span>
+                </li>
+              </ul>
+              <div className="top">
+                <span className="logo">Przedszkole nr 25</span>
+              </div>
             </div>
+          </div>
         </div>
-    )
+      </div>
+    </>
+  );
 }
 
+const SidebarPhone = () => {
+  if (isMobile) {
+    return <div> This content is available only on mobile</div>;
+  }
+  return <div> content... </div>;
+};
 
 export default Sidebar
