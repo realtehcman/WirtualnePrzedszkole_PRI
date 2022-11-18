29b58cb9cd6551d450b3d7d9973ac1ba7300f007
dam.strojny <d.strojny1@gmail.com>
2022-11-18 06:36:04 +0100

    Update App.js

--- frontend/wirtualneprzedszkole/src/App.js
+++ frontend/wirtualneprzedszkole/src/App.js
@@ -12,6 +12,12 @@ import ChildNavi from './pages/Children/ChildNavi';
 import LoginService from "./pages/Login/LoginService"
 import UserChildNavi from "./pages/UserChild/UserChildNavi"
 import EditUserNavi from './pages/EditUser/EditUserNavi';
+import MessageNavi from './pages/messages/MessageNavi';
+import SendMessageNavi from './pages/messages/SendMessageNavi';
+import SentMessageNavi from './pages/messages/SentMessageNavi';
+import ReadMessageNavi from './pages/messages/ReadMessageNavi';
+
+import React, {useState} from 'react';
 
 import {
   BrowserRouter,
@@ -23,53 +29,75 @@ import {
 
 
 function App() {
-  
+  const [navVisible, showNavbar] = useState(false);
+
   return (
-    <div className="App">
-    <BrowserRouter>
-      <Routes>
-        <Route path='/'>
-        <Route index element={<Login/>}></Route>
-        </Route>
-        <Route path="users" element={<PrivateOutlet />}>
-          <Route index element={<Users/>}></Route>
-        </Route>
-        <Route path='add-user' element={<PrivateOutlet />}>
-          <Route index element={<AddUser/>}></Route>
-        </Route>
-        <Route path='/home' element={<PrivateOutlet />}>
-          <Route index element={<Home/>}></Route>
-        </Route>
-        <Route path='/user/:id' element={<PrivateOutlet />}>
-          <Route index element={<UserNavi/>}></Route>
-        </Route>
-        <Route path='/groups' element={<PrivateOutlet />}>
-          <Route index element={<GroupNavi/>}></Route>
-        </Route>
-        <Route path='/group/:id' element={<PrivateOutlet />}>
-          <Route index element={<GroupIdNavi/>}></Route>
-        </Route>
-        <Route path='/add-group' element={<PrivateOutlet />}>
-          <Route index element={<AddGroup/>}></Route>
-        </Route>
-        <Route path="children" element={<PrivateOutlet />}>
-          <Route index element={<ChildrenNavi/>}></Route>
-        </Route>
-        <Route path='/add-child' element={<PrivateOutlet />}>
-          <Route index element={<AddChild/>}></Route>
-        </Route>
-        <Route path='/child/:id' element={<PrivateOutlet />}>
-          <Route index element={<ChildNavi/>}></Route>
-        </Route>
-        <Route path='/user/:id/child' element={<PrivateOutlet />}>
-          <Route index element={<UserChildNavi/>}></Route>
-        </Route>
-        <Route path="/user/:id/edit" element={<PrivateOutlet />}>
-          <Route index element={<EditUserNavi/>}></Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-    </div>
+      <div className="App">
+        <BrowserRouter>
+          <navbar visible={ navVisible } show={ showNavbar } />
+          <Routes>
+
+            <Route path="/home" element={<Navigate to="/home" />} />
+            <Route path='/home' element={
+              <div className={!navVisible ? "page" : "page page-with-navbar"}>
+                <h1>Profil</h1>
+              </div>
+            } />
+
+            <Route path='/'>
+              <Route index element={<Login/>}></Route>
+            </Route>
+            <Route path="users" element={<PrivateOutlet />}>
+              <Route index element={<Users/>}></Route>
+            </Route>
+            <Route path='add-user' element={<PrivateOutlet />}>
+              <Route index element={<AddUser/>}></Route>
+            </Route>
+            <Route path='/home' element={<PrivateOutlet />}>
+              <Route index element={<Home/>}></Route>
+            </Route>
+            <Route path='/user/:id' element={<PrivateOutlet />}>
+              <Route index element={<UserNavi/>}></Route>
+            </Route>
+            <Route path='/groups' element={<PrivateOutlet />}>
+              <Route index element={<GroupNavi/>}></Route>
+            </Route>
+            <Route path='/group/:id' element={<PrivateOutlet />}>
+              <Route index element={<GroupIdNavi/>}></Route>
+            </Route>
+            <Route path='/add-group' element={<PrivateOutlet />}>
+              <Route index element={<AddGroup/>}></Route>
+            </Route>
+            <Route path="children" element={<PrivateOutlet />}>
+              <Route index element={<ChildrenNavi/>}></Route>
+            </Route>
+            <Route path='/add-child' element={<PrivateOutlet />}>
+              <Route index element={<AddChild/>}></Route>
+            </Route>
+            <Route path='/child/:id' element={<PrivateOutlet />}>
+              <Route index element={<ChildNavi/>}></Route>
+            </Route>
+            <Route path='/user/:id/child' element={<PrivateOutlet />}>
+              <Route index element={<UserChildNavi/>}></Route>
+            </Route>
+            <Route path="/user/:id/edit" element={<PrivateOutlet />}>
+              <Route index element={<EditUserNavi/>}></Route>
+            </Route>
+            <Route path='/Message' element={<PrivateOutlet />}>
+              <Route index element={<MessageNavi/>}></Route>
+            </Route>
+            <Route path='/SendMessage' element={<PrivateOutlet />}>
+              <Route index element={<SendMessageNavi/>}></Route>
+            </Route>
+            <Route path='/SentMessage' element={<PrivateOutlet />}>
+              <Route index element={<SentMessageNavi/>}></Route>
+            </Route>
+            <Route path='/ReadMessage/:id' element={<PrivateOutlet />}>
+              <Route index element={<ReadMessageNavi/>}></Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
   );
 }
 
