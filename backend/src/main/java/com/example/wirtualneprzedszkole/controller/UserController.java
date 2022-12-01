328f128271782edddbf061aa2186777f54e931ee
Mateusz Naszko <60114149+LordIsak@users.noreply.github.com>
2022-12-01 17:20:53 +0100

    fix edit user in home page

--- backend/src/main/java/com/example/wirtualneprzedszkole/controller/UserController.java
+++ backend/src/main/java/com/example/wirtualneprzedszkole/controller/UserController.java
@@ -3,17 +3,21 @@ package com.example.wirtualneprzedszkole.controller;
 import com.example.wirtualneprzedszkole.mapper.UserMapper;
 import com.example.wirtualneprzedszkole.model.dto.RestartPasswordDto;
 import com.example.wirtualneprzedszkole.model.dto.UserDto;
+import com.example.wirtualneprzedszkole.service.UserManagementService;
 import com.example.wirtualneprzedszkole.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/user")
 public class UserController {
     private final UserService userService;
+    private final UserManagementService userManagementService;
 
     @PatchMapping("/restart")
     public void restartPassword(@RequestBody RestartPasswordDto restartPasswordDto) {
@@ -30,6 +34,12 @@ public class UserController {
     public UserDto getCurrentUser(Authentication authentication) {
         String email = authentication.getName();
         return UserMapper.mapToUserDto(userService.getCurrentUser(email));
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_TEACHER', 'ROLE_PARENT')")
+    @PutMapping("/current_user")
+    public UserDto updateCurrentUser(@Valid @RequestBody UserDto userDto) {
+        return UserMapper.mapToDto(userManagementService.updateUser(UserMapper.mapToDao(userDto)));
     }
 
 }
--- frontend/wirtualneprzedszkole/src/pages/Home/EditCurrent_User.js
+++ frontend/wirtualneprzedszkole/src/pages/Home/EditCurrent_User.js
@@ -5,8 +5,20 @@ import Current_User from "../Home/Home";
 import Current_UserService from "./Current_UserService";
 
 
-const EditCurrent_User = () => {
-    const [current_user, setCurrent_User] = useState({
+const EditCurrent_User = (props) => {
+    const current_user = {
+        id: props.id,
+        email: props.email,
+        name: props.name,
+        lastName: props.lastName,
+        phoneNumber: props.phoneNumber,
+        address: props.address
+    };
+
+    //const current_user = props
+    
+    //dodałem
+    const [userEdit, setUserEdit] = useState({
         id:'',
         email: '',
         name:'',
@@ -15,44 +27,77 @@ const EditCurrent_User = () => {
         address: ''
     });
 
-    let {id} = useParams()
 
-    useEffect(() => {
-        getData()
-    },[])
+    // useEffect(() => {
+    //     getData()
+    // },[])
 
 
-    const getData = async () => {
+    // const getData = async () => {
 
-        Current_UserService.getCurrent_User(id).then(response => {
-            console.log('Response from main API: ',response)
-            let current_userData = response.data;
-            setCurrent_User({id: current_userData.id, email: current_userData.email, name: current_userData.name, lastName: current_userData.lastName, phoneNumber: current_userData.phoneNumber,
-                address:current_userData.address
-            })
-        });
+    //     Current_UserService.getCurrent_User(id).then(response => {
+    //         console.log('Response from main API: ',response)
+    //         let current_userData = response.data;
+    //         setCurrent_User({id: current_userData.id, email: current_userData.email, name: current_userData.name, lastName: current_userData.lastName, phoneNumber: current_userData.phoneNumber,
+    //             address:current_userData.address
+    //         })
+    //     });
+    // }
+
+    // function putData()  {
+    //     console.log(current_user)
+    //     Current_UserService.EditCurrent_User(current_user)
+    // }
+
+
+    // return (
+    //     <div>
+    //         <form>
+    //             <label>Imię:</label><br></br>
+    //             <input placeholder={current_user.name} onChange={() => setCurrent_User({name: current_user.name})}/><br></br>
+    //             <label>Nazwisko:</label><br></br>
+    //             <input placeholder={current_user.lastName} onChange={() => setCurrent_User({lastName: current_user.lastName})}/><br></br>
+    //             <label>Email:</label><br></br>
+    //             <input placeholder={current_user.email} onChange={() => setCurrent_User({email: current_user.email})}/><br></br>
+    //             <label>Adres:</label><br></br>
+    //             <input placeholder={current_user.address} onChange={() => setCurrent_User({address: current_user.address})}/><br></br>
+    //             <label>Telefon:</label><br></br>
+    //             <input placeholder={current_user.phoneNumber} onChange={() => setCurrent_User({phoneNumber: current_user.phoneNumber})}/><br></br>
+    //             <button onClick={putData} className='btn btn-danger'>Zapisz</button>
+
+    //         </form>
+    //     </div>
+    // )
+
+
+    //dodałemn
+    const updateData = (e) => {
+        e.preventDefault()
+        userEdit.id = current_user.id
+        if (userEdit.name === "") userEdit.name = current_user.name
+        if (userEdit.lastName === "") userEdit.lastName = current_user.lastName
+        if (userEdit.email === "") userEdit.email = current_user.email
+        if (userEdit.phoneNumber === "") userEdit.phoneNumber = current_user.phoneNumber
+        if (userEdit.address === "") userEdit.address = current_user.address
+        console.log(userEdit)
+        Current_UserService.EditCurrent_User(userEdit)
     }
 
-    function putData()  {
-        console.log(current_user)
-        Current_UserService.EditCurrent_User(current_user)
-    }
-
-
+    //dodałem
     return (
         <div>
             <form>
-                <label>Imię:</label><br></br>
-                <input placeholder={current_user.name} onChange={() => setCurrent_User({name: current_user.name})}/><br></br>
-                <label>Nazwisko:</label><br></br>
-                <input placeholder={current_user.lastName} onChange={() => setCurrent_User({lastName: current_user.lastName})}/><br></br>
-                <label>Email:</label><br></br>
-                <input placeholder={current_user.email} onChange={() => setCurrent_User({email: current_user.email})}/><br></br>
-                <label>Adres:</label><br></br>
-                <input placeholder={current_user.address} onChange={() => setCurrent_User({address: current_user.address})}/><br></br>
-                <label>Telefon:</label><br></br>
-                <input placeholder={current_user.phoneNumber} onChange={() => setCurrent_User({phoneNumber: current_user.phoneNumber})}/><br></br>
-                <button onClick={putData} className='btn btn-danger'>Zapisz</button>
+            <label>Imię:</label><br></br>
+            <input placeholder={current_user.name} onChange={(e) => setUserEdit({...userEdit, name : e.target.value})}/><br></br>
+            <label>Nazwisko:</label><br></br>
+            <input  placeholder={current_user.lastName} onChange={(e) => setUserEdit({...userEdit, lastName : e.target.value})}/><br></br>
+            <label>Email:</label><br></br>
+            <input  placeholder={current_user.email} onChange={(e) => setUserEdit({...userEdit,email : e.target.value})}/><br></br>
+            <label>Adres:</label><br></br>
+            <input placeholder={current_user.address} onChange={(e) => setUserEdit({...userEdit, address : e.target.value})}/><br></br>
+            <label>Telefon:</label><br></br>
+            <input placeholder={current_user.phoneNumber} onChange={(e) => setUserEdit({...userEdit,phoneNumber : e.target.value})}/><br></br>
+            <button onClick={updateData} className='btn btn-danger'>Zapisz</button>
 
             </form>
         </div>
