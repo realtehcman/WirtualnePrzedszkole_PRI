8da46b15e52a08860a23b0aa1d04bad60e0813c9
CORP\mdmytren <max.dmytrenko@capgemini.com>
2023-01-09 00:02:32 +0100

    error

--- frontend/wirtualneprzedszkole/src/pages/gallery/GalleryNavi.js
+++ frontend/wirtualneprzedszkole/src/pages/gallery/GalleryNavi.js
@@ -1,21 +1,23 @@
 import React from "react";
-import "../User/Users.scss"
+import "../User/Users.scss";
 import Sidebar from "../../components/sidebar/sidebar";
-import Navbar from "../../components/navbar/navbar"
-import Gallery from "./Gallery"
-import {useNavigate, useParams} from "react-router-dom";
+import Navbar from "../../components/navbar/navbar";
+import Gallery from "./Gallery";
+import AddGallery from "./AddGallery.jsx";
+import { useNavigate, useParams } from "react-router-dom";
 
 const GalleryNavi = () => {
-    const navigate = useNavigate();
-    return (
-        <div className="gallery">
-            <Sidebar/>
-            <div className="usersContainer">
-                <Navbar/>
-                <Gallery/>
-            </div>
-        </div>
-    )
-}
+  const navigate = useNavigate();
+  return (
+    <div className="gallery">
+      <Sidebar />
+      <div className="usersContainer">
+        <AddGallery />
+        <Navbar />
+        <Gallery />
+      </div>
+    </div>
+  );
+};
 
-export default GalleryNavi
+export default GalleryNavi;
