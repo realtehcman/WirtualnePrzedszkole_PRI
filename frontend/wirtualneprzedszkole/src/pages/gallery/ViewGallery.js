489b581352fa3d701794a558e7fffdec32c7aa70
Damian Strojny <damstr1@st.amu.edu.pl>
2023-01-12 19:04:07 +0100

    Zaktualizuj 'frontend/wirtualneprzedszkole/src/pages/gallery/ViewGallery.js'

--- frontend/wirtualneprzedszkole/src/pages/gallery/ViewGallery.js
+++ frontend/wirtualneprzedszkole/src/pages/gallery/ViewGallery.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {useNavigate, useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
-import GalleryService from "../../../../../../../../Downloads/mati front/wirtualneprzedszkole_pri/frontend/wirtualneprzedszkole/src/pages/gallery/GalleryService";
+import GalleryService from "../gallery/GalleryService";
 import "../User/Table.scss";
 
 const ViewGallery = () => {
