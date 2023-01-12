801b6a70b9d312dd54c1cb613d051e76bb04bb9c
Damian Strojny <damstr1@st.amu.edu.pl>
2023-01-12 20:34:29 +0100

    Zaktualizuj 'frontend/wirtualneprzedszkole/src/pages/gallery/AddGalleryNavi.js'

--- frontend/wirtualneprzedszkole/src/pages/gallery/AddGalleryNavi.js
+++ frontend/wirtualneprzedszkole/src/pages/gallery/AddGalleryNavi.js
@@ -2,20 +2,20 @@ import React from "react";
 import "../User/Users.scss";
 import Sidebar from "../../components/sidebar/sidebar";
 import Navbar from "../../components/navbar/navbar";
-import AddGallery from "./AddGallery";
+import Gallery from "./Gallery";
 import { useNavigate, useParams } from "react-router-dom";
 
-const AddGalleryNavi = () => {
+const GalleryNavi = () => {
     const navigate = useNavigate();
     return (
         <div className="users gallery">
             <Sidebar />
             <div className="usersContainer">
                 <Navbar />
-                <AddGallery />
+                <Gallery />
             </div>
         </div>
     );
 };
 
-export default AddGalleryNavi;
+export default GalleryNavi;
