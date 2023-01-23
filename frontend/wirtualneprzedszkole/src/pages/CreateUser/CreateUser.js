bfe320d8b4d02856cb19871d45a1c4ff0d319f8a
Mateusz Naszko <60114149+LordIsak@users.noreply.github.com>
2023-01-23 19:39:24 +0100

    add parent or parent with child/children done but need a fix add other view for adding parent/other_role

--- frontend/wirtualneprzedszkole/src/pages/CreateUser/CreateUser.js
+++ frontend/wirtualneprzedszkole/src/pages/CreateUser/CreateUser.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import ChildrenService from "../Children/ChildrenService"
 import UserService from "../User/UserService";
 import "./CreateUser.scss";
 import { Link } from "react-router-dom";
@@ -13,7 +13,7 @@ import {
 import { display } from "@mui/system";
 
 
-  class CreateUser extends Component {
+class CreateUser extends Component {
     constructor(props) {
       super(props);
       this.state = {
@@ -30,6 +30,7 @@ import { display } from "@mui/system";
         children: [],
         display: 1
       };
+    
       this.changeNameHandler = this.changeNameHandler.bind(this);
       this.changeLastNameHandler = this.changeLastNameHandler.bind(this);
       this.changeEmailHandler = this.changeEmailHandler.bind(this);
@@ -38,11 +39,57 @@ import { display } from "@mui/system";
       this.changeRoleHandler = this.changeRoleHandler.bind(this);
       this.saveUser = this.saveUser.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
+      this.changeChildNameHandler = this.changeChildNameHandler.bind(this);
+      this.changeChildLastNameHandler = this.changeChildLastNameHandler.bind(this) 
     }
 
     saveUser = (e) => {
       e.preventDefault();
       console.log(e.nativeEvent.submitter.name)
+      if (e.nativeEvent.submitter.name === "save-user") {
+        let user = JSON.stringify({
+          email: this.state.email,
+          name: this.state.name,
+          lastName: this.state.lastName,
+          address: this.state.address,
+          phoneNumber: this.state.phoneNumber,
+          role: this.state.role,
+        });
+        //user = JSON.stringify(user)
+        //this.state.users.push(user)
+        
+        UserService.addUser(user).then((response) => {
+          console.log(response)
+        })
+        
+      }
+      else if (e.nativeEvent.submitter.name === "next-parent") {
+        let user = JSON.stringify({
+          email: this.state.email,
+          name: this.state.name,
+          lastName: this.state.lastName,
+          address: this.state.address,
+          phoneNumber: this.state.phoneNumber,
+          role: this.state.role,
+        });
+        //user = JSON.stringify(user)
+        //this.state.users.push(user)
+
+        this.setState({name: ""});
+        this.setState({lastName: ""});
+        this.setState({email: ""});
+        this.setState({address: ""});
+        this.setState({phoneNumber: ""});
+      //this.setState({display: 0})
+        console.log(this.state.users)
+        console.log(this.state.display)
+        UserService.addUser(user).then((response) => {
+          console.log(response)
+          this.state.users.push(response.data)
+        })
+    }
+
+    else if (e.nativeEvent.submitter.name === "add-child") {
       let user = JSON.stringify({
         email: this.state.email,
         name: this.state.name,
@@ -52,17 +99,69 @@ import { display } from "@mui/system";
         role: this.state.role,
       });
       //user = JSON.stringify(user)
-      this.state.users.push(user)
+      //this.state.users.push(user)
+        this.setState({display: 0})
+        /* let child = {
+          name: this.state.childName,
+          lastName: this.state.childLastName,
+          
+        };
+        this.state.children.push(child)
+        console.log(this.state.children) */
+        console.log(this.state.users)
+        UserService.addUser(user).then((response) => {
+          console.log(response)
+          this.state.users.push(response.data)
+        })
+    } else if (e.nativeEvent.submitter.name === "next-child") {
 
-      this.setState({name: ""});
-      this.setState({lastName: ""});
-      this.setState({email: ""});
-      this.setState({address: ""});
-      this.setState({phoneNumber: ""});
-      this.setState({role: ""});
-      //this.setState({display: 0})
+      let child = JSON.stringify({
+        name: this.state.childName,
+        lastName: this.state.childLastName,
+        
+      });
+      //this.state.children.push(child)
+
+      this.setState({childName: ""});
+      this.setState({childLastName: ""});
+
+      console.log(this.state.children)
       console.log(this.state.users)
-      console.log(this.state.display)
+      ChildrenService.addChild(child).then((response) => {
+        console.log(response)
+        this.state.children.push(response.data)
+      })
+    } else {
+      let child = JSON.stringify({
+        name: this.state.childName,
+        lastName: this.state.childLastName,
+        
+      });
+      //this.state.children.push(child)
+      
+      ChildrenService.addChild(child).then((response) => {
+        console.log(response)
+        this.state.children.push(response.data)
+        this.state.users.forEach((parent) => {
+          this.state.children.forEach((child) => {
+            UserService.addChildToUser(parent.id, child).then((response) => {
+              console.log(response)
+            })
+          })
+        })
+      })
+
+      console.log(this.state.users)
+
+      /* this.state.users.forEach((parent) => {
+        this.state.children.forEach((child) => {
+          UserService.addChildToUser(parent.id, child).then((response) => {
+            console.log(response)
+          })
+        })
+      }) */
+
+    }
       /* UserService.addUser(user).then((response) => {
         if (response.data != null) {
           this.setState(this.state);
@@ -98,17 +197,26 @@ import { display } from "@mui/system";
       e.preventDefault();
     }
 
+    changeChildNameHandler = (event) => {
+      this.setState({childName: event.target.value})
+    }
+
+    changeChildLastNameHandler = (event) => {
+      this.setState({childLastName: event.target.value})
+    }
+
     render() {
       const display = this.state.display
       if (display === 1) {
-      return (
+        if (this.state.role === "PARENT") {
+          return (
           <div className="formContainer">
             <div className="row">
               <div className="card col-md-6 offset-md-3 offset-md-3">
                 <div className="form-body">
                   <form onSubmit={(e) =>{
                     this.saveUser(e);
-                    /* this.handleSubmit(e); */
+                    this.handleSubmit(e);
                   } }>
                     <div className="form-group">
                       <MDBInput
@@ -184,17 +292,113 @@ import { display } from "@mui/system";
                       </Link>
                     </div>
                     <div className="asd124">
-                      <button className="button" name="zapisz">Zapisz</button>
+                      <button className="button" name="save-user">Zapisz</button>
                     </div>
-                    <div className="next-parent">
-                      <button className="button">Dodaj następnego opiekuna</button>
+                    <div className="next-parent-class">
+                      <button className="button" name="next-parent">Dodaj następnego opiekuna</button>
+                    </div>
+                    <div className="add-child-class">
+                      <button className="button" name="add-child">Dodaj dziecko</button>
                     </div>
                   </form>
                 </div>
               </div>
             </div>
           </div>
-      );
+          );
+        } else {
+          return (
+            <div className="formContainer">
+              <div className="row">
+                <div className="card col-md-6 offset-md-3 offset-md-3">
+                  <div className="form-body">
+                    <form onSubmit={(e) =>{
+                      this.saveUser(e);
+                      this.handleSubmit(e);
+                    } }>
+                      <div className="form-group">
+                        <MDBInput
+                            placeholder="Imię"
+                            type="text"
+                            required
+                            name="Imię"
+                            className='"form-control'
+                            value={this.state.name}
+                            onChange={this.changeNameHandler}
+                        />
+                      </div>
+                      <div className="form-group">
+                        <MDBInput
+                            placeholder="Nazwisko"
+                            type="text"
+                            required
+                            name="Nazwisko"
+                            className='"form-control'
+                            value={this.state.lastName}
+                            onChange={this.changeLastNameHandler}
+                        />
+                      </div>
+                      <div className="form-group">
+                        <MDBInput
+                            placeholder="Email"
+                            type="email"
+                            required
+                            name="Email"
+                            className='"form-control'
+                            value={this.state.email}
+                            onChange={this.changeEmailHandler}
+                        />
+                      </div>
+                      <div className="form-group">
+                        <MDBInput
+                            placeholder="Adres"
+                            required
+                            name="Adres"
+                            className='"form-control'
+                            value={this.state.address}
+                            onChange={this.changeAddressHandler}
+                        />
+                      </div>
+                      <div className="form-group">
+                        <MDBInput
+                            placeholder="Numer Telefonu"
+                            type="tel"
+                            pattern="[0-9]{3}[0-9]{3}[0-9]{3}"
+  
+                            required
+                            name="Numer Telefonu"
+  
+                            value={this.state.phoneNumber}
+                            onChange={this.changePhoneHandler}
+                        />
+                      </div>
+                      <div className="form-group">
+                        <select
+                            id="lang"
+                            value={this.state.role}
+                            onChange={this.changeRoleHandler}
+                        >
+                          <option value="PARENT">Rodzic</option>
+                          <option value="TEACHER">Nauczyciel</option>
+                          <option value="ADMIN">Administrator</option>
+                        </select>
+                      </div>
+                      <div className="asd123">
+  
+                        <Link className="button" to={"/users"}>
+                          Wróć
+                        </Link>
+                      </div>
+                      <div className="asd124">
+                        <button className="button" name="save-user">Zapisz</button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+            );
+        }
     } else {
       return (
         <div className="formContainer">
@@ -203,7 +407,7 @@ import { display } from "@mui/system";
               <div className="form-body">
                 <form onSubmit={(e) =>{
                   this.saveUser(e);
-                  this.handleSubmit(e);
+                  //this.handleSubmit(e);
                 } }>
                   <div className="form-group">
                     <MDBInput
@@ -212,8 +416,8 @@ import { display } from "@mui/system";
                         required
                         name="imię"
                         className='"form-control'
-                        value={this.state.name}
-                        onChange={this.changeNameHandler}
+                        value={this.state.childName}
+                        onChange={this.changeChildNameHandler}
                     />
                   </div>
                   <div className="form-group">
@@ -223,10 +427,21 @@ import { display } from "@mui/system";
                         required
                         name="Nazwisko"
                         className='"form-control'
-                        value={this.state.lastName}
-                        onChange={this.changeLastNameHandler}
+                        value={this.state.childLastName}
+                        onChange={this.changeChildLastNameHandler}
                     />
                   </div>
+                  {/* <div className="form-group">
+                    <MDBInput
+                        placeholder="Klasa"
+                        type="text"
+                        required
+                        name="Nazwisko"
+                        className='"form-control'
+                        value={this.state.childLastName}
+                        onChange={this.changeLastNameHandler}
+                    />
+                  </div> */}
                   <div className="asd123">
 
                     <Link className="button" to={"/users"}>
@@ -234,10 +449,10 @@ import { display } from "@mui/system";
                     </Link>
                   </div>
                   <div className="asd124">
-                    <button className="button">Zapisz</button>
+                    <button className="button" name="save-with-child">Zapisz</button>
                   </div>
-                  <div className="next-child">
-                    <button className="button">Dodaj następne dziecko</button>
+                  <div className="next-child-class">
+                    <button className="button" name="next-child">Dodaj następne dziecko</button>
                   </div>
                 </form>
               </div>
