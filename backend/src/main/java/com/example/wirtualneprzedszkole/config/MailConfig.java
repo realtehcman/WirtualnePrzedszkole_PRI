b926abdca7fba3340fd8b551eba112616d4fc9c8
Maksym Dmyterko <max.dmytrenko@capgemini.com>
2023-01-27 09:13:43 +0100

    fix JavaMailSender

--- backend/src/main/java/com/example/wirtualneprzedszkole/config/MailConfig.java
+++ backend/src/main/java/com/example/wirtualneprzedszkole/config/MailConfig.java
@@ -5,12 +5,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
+import java.util.Properties;
+
 @Configuration
 public class MailConfig {
 
     @Bean
     public JavaMailSender javaMailSender() {
-        return new JavaMailSenderImpl();
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername("wirtualne.przedszkole25@gmail.com");
+        mailSender.setPassword("izxsmkfgnzdzjxcm");
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+        return mailSender;
     }
 
 }
