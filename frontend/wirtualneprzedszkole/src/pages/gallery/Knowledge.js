413d94023d4ae67b92aaea350e2ad260daa22c95
Mateusz Naszko <60114149+LordIsak@users.noreply.github.com>
2023-02-23 20:10:34 +0100

    fix Knowledge

--- frontend/wirtualneprzedszkole/src/pages/gallery/Knowledge.js
+++ frontend/wirtualneprzedszkole/src/pages/gallery/Knowledge.js
@@ -1,6 +1,6 @@
 import FileService from "../gallery/FileService";
 import "../gallery/Knowledge.scss";
-import React, { useEffect, useState } from 'react'
+import React, {useEffect, useState} from 'react'
 import saveAs from 'file-saver'
 import EditFile from "./EditFile";
 import Popup from "../GroupDisplay/Popup";
@@ -9,10 +9,8 @@ import HeightIcon from '@mui/icons-material/Height';
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import currentUserService from "../Home/CurrentUserService";
-import { useTranslation } from "react-i18next";
 
 const Knowledge = () => {
-    const {t} = useTranslation();
 
     const [filesInfo, setFilesInfo] = useState([
         {
@@ -35,14 +33,14 @@ const Knowledge = () => {
 
     useEffect(() => {
         getData().then(r => console.log(r))
-        // eslint-disable-next-line
-    }, [])
+    // eslint-disable-next-line
+    },[])
 
 
     const getData = async () => {
         currentUserService.getCurrentUsers().then(response => {
             let currentUserData = response.data;
-            setCurrentUser({ id: currentUserData.id, role: currentUserData.role })
+            setCurrentUser({id: currentUserData.id, role: currentUserData.role})
         });
     }
 
@@ -62,37 +60,37 @@ const Knowledge = () => {
 
     useEffect(() => {
         const getKnowledge = async () => {
-            FileService.getKnowledge().then((response) => {
-                let responseFiles = response.data;
-                if (sortBy === "name") {
-                    responseFiles.sort((a, b) => {
-                        if (sortOrder === "asc") {
-                            return (a.name > b.name) ? 1 : -1;
-                        } else {
-                            return (a.name < b.name) ? 1 : -1;
-                        }
-                    });
-                } else if (sortBy === "dateAdded") {
-                    responseFiles.sort((a, b) => {
-                        if (sortOrder === "asc") {
-                            return new Date(a.dateAdded) - new Date(b.dateAdded);
-                        } else {
-                            return new Date(b.dateAdded) - new Date(a.dateAdded);
-                        }
-                    });
-                } else {
-                    responseFiles.sort((a, b) => a.id - b.id);
-                }
-                // eslint-disable-next-line
-                responseFiles.map((file) => {
-                    if (file.dateAdded != null)
-                        file.dateAdded = (new Date(file.dateAdded)).toISOString().split('T')[0]
+        FileService.getKnowledge().then((response) => {
+            let responseFiles = response.data;
+            if (sortBy === "name") {
+                responseFiles.sort((a, b) => {
+                    if (sortOrder === "asc") {
+                        return (a.name > b.name) ? 1 : -1;
+                    } else {
+                        return (a.name < b.name) ? 1 : -1;
+                    }
                 });
-                setFilesInfo(responseFiles);
-            }).catch((reason) => {
-                console.log(`axios request failed: ${reason}`);
+            } else if (sortBy === "dateAdded") {
+                responseFiles.sort((a, b) => {
+                    if (sortOrder === "asc") {
+                        return new Date(a.dateAdded) - new Date(b.dateAdded);
+                    } else {
+                        return new Date(b.dateAdded) - new Date(a.dateAdded);
+                    }
+                });
+            } else {
+                responseFiles.sort((a, b) => a.id - b.id);
+            }
+             // eslint-disable-next-line
+            responseFiles.map((file) => {
+                if (file.dateAdded != null)
+                    file.dateAdded = (new Date(file.dateAdded)).toISOString().split('T')[0]
             });
-        };
+            setFilesInfo(responseFiles);
+        }).catch((reason) => {
+            console.log(`axios request failed: ${reason}`);
+        });
+    };
         getKnowledge();
     }, [sortBy, sortOrder]);
 
@@ -100,10 +98,10 @@ const Knowledge = () => {
     const printFiles = async (file) => {
         FileService.getFile(KNOWLEDGE_ID, file.hash).then((response) => {
             saveAs(response.data, file.name)
-        })
+      })
     }
 
-    const handleSubmit = async (event) => {
+    const handleSubmit = async(event) => {
         event.preventDefault()
 
         const formData = new FormData(event.currentTarget);
@@ -117,7 +115,7 @@ const Knowledge = () => {
             if (response.status === 200) {
                 console.log(response.data[0])
                 let responseFiles = response.data
-                // eslint-disable-next-line
+                 // eslint-disable-next-line
                 responseFiles.map((file) => {
                     if (file.dateAdded != null)
                         file.dateAdded = (new Date(file.dateAdded)).toISOString().split('T')[0]
@@ -170,7 +168,7 @@ const Knowledge = () => {
         return text;
     }
 
-    const [buttonPopup, setButtonPopup] = useState({
+    const[buttonPopup, setButtonPopup] = useState({
         isPop: false,
         fileId: "",
         description: ""
@@ -189,63 +187,60 @@ const Knowledge = () => {
 
 
     return (
-        <div data-testid="knowledge">
+        <div data-testid="knowledge"
+             className="scrollable-div">
             <ToastContainer />
-            <div className="mb-4">
+            <div className="abc">
                 <form>
-                    <input type="text" className="form-control border-0" placeholder={t('search_for_files_by_name')} onChange={handleSearch} />
+                    <input type="text" placeholder="szukaj plików po nazwie" onChange={handleSearch} />
                 </form>
             </div>
 
-            <div className="scrollable-div">
-                <table className="content-table w-100">
-                    <thead>
-                        <tr className="table-head">
-                            <td>
-                                <SortIcon className="icon" onClick={handleSortByName} />
-                                <span className="text">{t('file')}</span>
-                            </td>
 
-                            <td className="icon-text">
-                                <HeightIcon className="icon" onClick={handleSortByDate} />
-                                <span className="text">{t('data')}</span>
-                            </td>
+            <table className="content-table">
+                <thead>
+                    <tr className="table-head">
+                        <td>
+                            <SortIcon className="icon" onClick={handleSortByName}/>
+                            <span className="text">Plik</span>
+                        </td>
 
-                            {currentUser.role === "ADMIN" && <td>{t('description')}</td>}
-                            <td>{t('download')}</td>
-                            {currentUser.role === "ADMIN" && <td>{t('delete')}</td>}
+                        <td className="icon-text">
+                            <HeightIcon className="icon" onClick={handleSortByDate}/>
+                            <span className="text">Data</span>
+                        </td>
+
+                        {currentUser.role === "ADMIN" && <td>Opis</td>}
+                        <td>Pobierz</td>
+                        {currentUser.role === "ADMIN" && <td>Usuń</td>}
+                    </tr>
+                </thead>
+                <tbody className="body table-body">
+                {filteredFiles.map((file) => (
+
+                    <tr key = {file.id}>
+                            <td id="tooltip">{file.name}<td id="hiddenText">{displayHiddentText(file.description)}</td></td>
+                            <td>{checkDataIsNull(file.dateAdded)}</td>
+                        {currentUser.role === "ADMIN" &&    <td><button type="button" className='btn btn-info' onClick={() => setButtonPopup({isPop: true, fileId: file.id, description: file.description})}>Edytuj</button></td>}
+                            <td><button size="lg" className="btn btn-primary" onClick={() => printFiles(file)}>Pobierz</button></td>
+                        {currentUser.role === "ADMIN" && <td><button onClick={() => deleteFile(file)} className="btn btn-danger">Usuń</button></td>}
+
                         </tr>
-                    </thead>
-                    <tbody className="body table-body">
-                        {filteredFiles.map((file) => (
-
-                            <tr key={file.id}>
-                                <td id="tooltip">{file.name}<td id="hiddenText">{displayHiddentText(file.description)}</td></td>
-                                <td>{checkDataIsNull(file.dateAdded)}</td>
-                                {currentUser.role === "ADMIN" && <td><button type="button" className='btn btn-info' onClick={() => setButtonPopup({ isPop: true, fileId: file.id, description: file.description })}>{t('edit')}</button></td>}
-                                <td><button size="lg" className="btn btn-primary" onClick={() => printFiles(file)}>{t('download')}</button></td>
-                                {currentUser.role === "ADMIN" && <td><button onClick={() => deleteFile(file)} className="btn btn-danger">{t('delete')}</button></td>}
-
-                            </tr>
-                        ))}
-                        <Popup trigger={buttonPopup.isPop} setTrigger={setButtonPopup}><EditFile  {...buttonPopup} /></Popup>
-                    </tbody>
-                </table>
-            </div>
-
+                    ))}
+                    <Popup trigger={buttonPopup.isPop} setTrigger={setButtonPopup}><EditFile  {...buttonPopup}/></Popup>
+                </tbody>
+            </table>
             <br />
-            {currentUser.role === "ADMIN" && 
-            <div>
+            {currentUser.role === "ADMIN" &&      <div className="uploadDiv">
                 <form onSubmit={handleSubmit} encType='multipart/form-data'>
-                    <div> 
-                          <input type="file" className="form-control" id="customFile" name='file' multiple />
-                    </div>
+                 <div className="input23">   <input type="file" className="form-control" id="customFile" name='file' multiple/></div>
+                    <p></p>
+                    <button type="submit" className="btn btn-primary"> Wyślij</button>
                 </form>
             </div>}
 
-            <div className="d-md-flex align-items-center justify-content-between mt-5">
-                <button type="submit" className="btn btn_global">{t('send')}</button>
-                {currentUser.role === "ADMIN" && <button onClick={() => deleteAllFiles()} className="btn btn-danger btn-lg">{t('delete_all_files')}</button>}
+            <div className="deleteAll">
+                {currentUser.role === "ADMIN" && <button onClick={() => deleteAllFiles()} className="btn btn-danger btn-lg">Usuń wszystkie pliki</button>}
             </div>
 
         </div>
@@ -253,3 +248,4 @@ const Knowledge = () => {
 }
 
 export default Knowledge
+
