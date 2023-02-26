7890e03601d7b2bea9462660ba2c07e3aa6da41e
Mateusz Naszko <60114149+LordIsak@users.noreply.github.com>
2023-02-26 15:23:04 +0100

    add className to tabel 2

---
+++ backend/src/main/java/com/example/wirtualneprzedszkole/model/dto/ChildWithClassNameDto.java
@@ -0,0 +1,22 @@
+package com.example.wirtualneprzedszkole.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ChildWithClassNameDto {
+    private Long id;
+    private Long classId;
+    private String className;
+    private String name;
+    private String lastName;
+
+    private List<UserDto> parents;
+}
