1c338c236ae5b0c9999f5ae1be21c6993557e705
CORP\mdmytren <max.dmytrenko@capgemini.com>
2023-02-28 20:25:45 +0100

    feature: add translation

--- frontend/wirtualneprzedszkole/src/components/navbar/navbar.js
+++ frontend/wirtualneprzedszkole/src/components/navbar/navbar.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
 import "./navbar.scss"
 import LogoutIcon from '@mui/icons-material/Logout';
-import {Link, useNavigate} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import LoginService from "../../pages/Login/LoginService.js";
 import i18next from 'i18next';
 import { useTranslation } from "react-i18next";
 
 
 const Navbar = () => {
+    const {t} = useTranslation();
+    const [currentLanguage, setCurrentLanguage] = useState(i18next.language);
     const navigate = useNavigate();
 
     const logout = async (e) => {
@@ -16,20 +18,27 @@ const Navbar = () => {
         navigate("/")
     }
 
-
+    const onchangeLanguage = (e) => {
+        setCurrentLanguage(e.target.value);
+        i18next.changeLanguage(e.target.value);
+    }
 
     return (
         <div data-testid="navbar" className="navbar p-0">
             <div className="App_card">
-                <div className="wrapper d-flex justify-content-end align-items-center">
-
-                    <div className="logout btn">
-                        <LogoutIcon/>
-                        <span onClick={logout}>Wyloguj się</span>
-
-                    </div>
-
+            <div className="wrapper d-flex justify-content-end align-items-center">
+                <div className="me-4">
+                    <select value={currentLanguage} className="form-select border-0" onChange={(e) => onchangeLanguage(e)}>
+                        <option value="pl">Polish</option>
+                        <option value="en">English</option>
+                        <option value="ua">Ukrainian</option>
+                    </select>
                 </div>
+                <div className="logout btn">
+                    <LogoutIcon/>
+                    <span onClick={logout}>{t('log_out')}</span>
+                </div>
+            </div>
             </div>
         </div>
     )
