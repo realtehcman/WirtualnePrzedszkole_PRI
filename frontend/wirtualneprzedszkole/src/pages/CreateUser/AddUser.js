f58b9e1bc0d1ed5c354881c29d7568ffc517c7c0
dam.strojny <d.strojny1@gmail.com>
2023-03-02 20:47:01 +0100

    add user - role

--- frontend/wirtualneprzedszkole/src/pages/CreateUser/AddUser.js
+++ frontend/wirtualneprzedszkole/src/pages/CreateUser/AddUser.js
@@ -4,10 +4,24 @@ import Sidebar from "../../components/sidebar/sidebar";
 import Navbar from "../../components/navbar/navbar"
 import CreateUser from "./CreateUser";
 import { useTranslation } from "react-i18next";
+import {useContext} from "react";
+import UserContext from "../../components/sidebar/UserContext";
 
 const AddUser = () => {
     const {t} = useTranslation();
+    const currentUser = useContext(UserContext);
 
+    if (currentUser.role === "PARENT" || currentUser.role === "TEACHER" ) {
+        return (
+            <div data-testid="users" className="users">
+                <Sidebar />
+                <div data-testid="usersContainer" className="usersContainer">
+                    <Navbar />
+                    <p><h1>You don't have permission to access this page.</h1></p>
+                </div>
+            </div>
+        );
+    }
     return (
         <div className="addusers">
             <Sidebar/>
